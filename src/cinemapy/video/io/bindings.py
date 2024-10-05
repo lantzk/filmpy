e1a02e227e4b98f106de51a5d9f28cc425914e9d
lantzk <9219265+lantzk@users.noreply.github.com>
2024-10-05 11:14:29 -0500

    switch to buffer_rgba

--- src/cinemapy/video/io/bindings.py
+++ src/cinemapy/video/io/bindings.py
@@ -7,7 +7,6 @@ import numpy as np
 
 def mplfig_to_npimage(fig):
     """Converts a matplotlib figure to a RGB frame after updating the canvas."""
-    #  only the Agg backend now supports the tostring_rgb function
     from matplotlib.backends.backend_agg import FigureCanvasAgg
 
     canvas = FigureCanvasAgg(fig)
@@ -17,7 +16,8 @@ def mplfig_to_npimage(fig):
     _l, _b, w, h = canvas.figure.bbox.bounds
     w, h = int(w), int(h)
 
-    #  exports the canvas to a string buffer and then to a numpy nd.array
-    buf = canvas.tostring_rgb()
-    image = np.frombuffer(buf, dtype=np.uint8)
-    return image.reshape(h, w, 3)
+    #  exports the canvas to a numpy array using buffer_rgba
+    buf = canvas.buffer_rgba()
+    # Convert RGBA to RGB
+    image = np.asarray(buf)[:, :, :3]
+    return image
